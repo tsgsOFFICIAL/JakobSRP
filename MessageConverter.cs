958a0ac9e6de61813e41d83ac6b50c1700caf24d
Marcus Mortensen Jensen <marc7075@zbc.dk>
2021-06-10 10:41:53 +0200

    SOLID

--- MessageConverter.cs
+++ MessageConverter.cs
@@ -9,10 +9,10 @@ namespace JakobSRP
     public abstract class MessageConverter
     {
         /// <summary>
-        /// 
+        /// Converts a string body to HTML
         /// </summary>
-        /// <param name="plainText"></param>
-        /// <returns></returns>
+        /// <param name="plainText">String body</param>
+        /// <returns>This method returns the converted body, as a string with apropriate HTML tags</returns>
         public static string ConvertBodyToHTML(string plainText)
         {
             return $"{plainText}";
